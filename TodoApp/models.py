475967d99195e8ba4b4d25fef6ff9bf83a015f99
Ozer, Ozdal <ozerozdal@gmail.com>
2024-01-22 11:53:30 -0500

    adding main.py to create database connection for API

--- TodoApp/models.py
+++ TodoApp/models.py
@@ -9,5 +9,4 @@ class Todos(Base):
     title = Column(String)
     description = Column(String)
     priority = Column(Integer)
-    complete =  Column(Boolean, default=False)
-    
+    complete = Column(Boolean, default=False)
