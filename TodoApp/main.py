38c8ab8f10f474d06e99d6a19332518133d02f39
Ozer, Ozdal <ozerozdal@gmail.com>
2024-01-23 16:51:52 -0500

    get todo by ID

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,6 +1,7 @@
 from typing import Annotated
 from sqlalchemy.orm import Session
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, Path
+from starlette import status
 import models
 from models import Todos
 from database import engine, SessionLocal
@@ -22,10 +23,22 @@ def get_db():
         db.close()
 
 
-@app.get("/")
-async def read_all(db: Annotated[Session, Depends(get_db)]):
+dp_dependency = Annotated[Session, Depends(get_db)]
+
+
+@app.get("/", status_code=status.HTTP_200_OK)
+async def read_all(db: dp_dependency):
     return db.query(Todos).all()
-        
+
+
+@app.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
+async def read_todo(db: dp_dependency, todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is not None:
+        return todo_model
+    raise HTTPException(status_code=404, detail='Todo not found')
+
+
 #if __name__ == "__main__":
     # Start uvicorn
     # uvicorn.run(app, host="127.0.0.1", port=5050)
