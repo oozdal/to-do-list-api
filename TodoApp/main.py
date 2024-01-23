31407520ed11856577189be9e0756de379d2f4ef
Ozer, Ozdal <ozerozdal@gmail.com>
2024-01-23 15:57:37 -0500

    get all todos from database

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,8 +1,31 @@
-from fastapi import FastAPI
+from typing import Annotated
+from sqlalchemy.orm import Session
+from fastapi import FastAPI, Depends
 import models
-from database import engine
+from models import Todos
+from database import engine, SessionLocal
+import uvicorn
 
 
+# Create FastAPI object
 app = FastAPI()
 
+
 models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/")
+async def read_all(db: Annotated[Session, Depends(get_db)]):
+    return db.query(Todos).all()
+        
+#if __name__ == "__main__":
+    # Start uvicorn
+    # uvicorn.run(app, host="127.0.0.1", port=5050)
