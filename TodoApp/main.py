093f5c81afc2667bfe3d8e57c741ce8c3ef90c9f
Ozer, Ozdal <ozerozdal@gmail.com>
2024-01-23 19:31:42 -0500

    adding a post request

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,4 +1,5 @@
 from typing import Annotated
+from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
 from fastapi import FastAPI, Depends, HTTPException, Path
 from starlette import status
@@ -26,6 +27,13 @@ def get_db():
 dp_dependency = Annotated[Session, Depends(get_db)]
 
 
+class TodoRequest(BaseModel):
+    title: str = Field(min_length=3)
+    description: str = Field(min_length=3, max_length=100)
+    priority: int = Field(gt=0, lt=6)
+    complete: bool = Field
+
+
 @app.get("/", status_code=status.HTTP_200_OK)
 async def read_all(db: dp_dependency):
     return db.query(Todos).all()
@@ -38,6 +46,12 @@ async def read_todo(db: dp_dependency, todo_id: int = Path(gt=0)):
         return todo_model
     raise HTTPException(status_code=404, detail='Todo not found')
 
+
+@app.post("/todo", status_code=status.HTTP_201_CREATED)
+async def create_todo(db: dp_dependency, todo_request: TodoRequest):
+    todo_model = Todos(**todo_request.dict())
+    db.add(todo_model)
+    db.commit()
 
 #if __name__ == "__main__":
     # Start uvicorn
