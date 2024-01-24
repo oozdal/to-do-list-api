302122a48de883f4892a3f4dbbbfc849774d4e40
Ozer, Ozdal <ozerozdal@gmail.com>
2024-01-24 11:28:55 -0500

    adding a put request

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -50,8 +50,27 @@ async def read_todo(db: dp_dependency, todo_id: int = Path(gt=0)):
 @app.post("/todo", status_code=status.HTTP_201_CREATED)
 async def create_todo(db: dp_dependency, todo_request: TodoRequest):
     todo_model = Todos(**todo_request.dict())
+    
     db.add(todo_model)
     db.commit()
+
+
+@app.put("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def update_todo(db: dp_dependency,
+                      todo_request: TodoRequest,
+                      todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is None:
+        raise HTTPException(status_code=404, detail='Todo not found')
+    
+    todo_model.title = todo_request.title
+    todo_model.description = todo_request.description
+    todo_model.priority = todo_request.priority
+    todo_model.complete = todo_request.complete
+
+    db.add(todo_model)
+    db.commit()
+    
 
 #if __name__ == "__main__":
     # Start uvicorn
