236d5ae3720100c594e3bc5946a2425072c6d8e7
Ozer, Ozdal <ozerozdal@gmail.com>
2024-02-02 16:10:31 -0500

    create a MySQL database

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -4,11 +4,11 @@ from sqlalchemy.ext.declarative import declarative_base
 
 
 #SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db' # sqlite db
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:test1234!@localhost/TodoApplicationDatabase' # postgres db
-
+#SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:test1234!@localhost/TodoApplicationDatabase' # postgres db
+SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:test1234!@127.0.0.1:3306/TodoApplicationDatabase' # mysql db
 
 #engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False}) # sqlite db
-engine = create_engine(SQLALCHEMY_DATABASE_URL) # postgres db
+engine = create_engine(SQLALCHEMY_DATABASE_URL) # postgres db or mysql db
 
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
