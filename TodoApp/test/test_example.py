baf8005fb29bd59c3b6544fbbe0c246405d5ab34
Ozer, Ozdal <ozerozdal@gmail.com>
2024-02-05 17:39:34 -0500

    generate some Pytest objects

--- TodoApp/test/test_example.py
+++ TodoApp/test/test_example.py
@@ -1,3 +1,4 @@
+import pytest
 
 def test_equal_or_not_equal():
     assert 3 == 3
@@ -32,3 +33,24 @@ def test_list():
     assert 7 not in num_list
     assert all(num_list)
     assert not any(any_list)
+
+
+class Student:
+    def __init__(self, first_name: str, last_name: str, major: str, years: int):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.major = major
+        self.years = years
+
+
+@pytest.fixture
+def default_employee():
+    return Student('John', 'Doe', 'Physics', 3)
+
+
+def test_person_initialization(default_employee):
+    assert default_employee.first_name == 'John' # First name should be John
+    assert default_employee.last_name == 'Doe' # Last name should be Doe
+    assert default_employee.major == 'Physics'
+    assert default_employee.years == 3
+        
