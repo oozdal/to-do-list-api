a83a19d51a89c3e4470e795d9e297cc2c949b68f
Ozer, Ozdal <ozerozdal@gmail.com>
2024-02-09 23:22:44 -0500

    adding test_admin_delete and test_admin_delete_todo_not_found

--- TodoApp/test/test_admin.py
+++ TodoApp/test/test_admin.py
@@ -1,6 +1,7 @@
 from .utils import *
 from ..routers.admin import get_db, get_current_user
 from fastapi import status
+from ..models import Todos
 
 
 app.dependency_overrides[get_db] = override_get_db
@@ -14,3 +15,17 @@ def test_admin_read_all_authenticated(test_todo):
                                 'description': 'Need to learn everyday', 'id': 1,
                                 'priority': 5, 'owner_id': 1}]
 
+
+def test_admin_delete(test_todo):
+    response = client.delete("/admin/todo/1")
+    assert response.status_code == 204
+
+    db = TestingSessionLocal()
+    model = db.query(Todos).filter(Todos.id == 1).first()
+    assert model is None
+
+
+def test_admin_delete_todo_not_found():
+    response = client.delete("/admin/todo/9999")
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'Todo not found.'}
