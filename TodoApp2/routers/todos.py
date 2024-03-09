8cc1f7d259bf1096716e7bbfcc453808af38d7af
Ozer, Ozdal <ozerozdal@gmail.com>
2024-03-08 23:42:00 -0500

    update add-todo.html and edit-todo.html

--- TodoApp2/routers/todos.py
+++ TodoApp2/routers/todos.py
@@ -35,7 +35,14 @@ def get_db():
 
 @router.get("/", response_class=HTMLResponse)
 async def read_all_by_user(request: Request, db: Session = Depends(get_db)):
-    return templates.TemplateResponse("home.html", {"request": request})
+
+    #user = await get_current_user(request)
+    #if user is None:
+    #    return RedirectResponse(url="/auth", status_code=status.HTTP_302_FOUND)
+
+    todos = db.query(models.Todos).filter(models.Todos.owner_id == 1).all()
+
+    return templates.TemplateResponse("home.html", {"request": request, 'todos': todos})
 
 
 @router.get("/add-todo", response_class=HTMLResponse)
@@ -43,9 +50,34 @@ async def add_new_todo(request: Request):
     return templates.TemplateResponse("add-todo.html", {"request": request})
 
 
+@router.post("/add-todo", response_class=HTMLResponse)
+async def create_todo(request: Request, title: str = Form(...), description: str = Form(...),
+                    priority: int = Form(...), db: Session = Depends(get_db)):
+    
+    #user = await get_current_user(request)
+    #if user is None:
+    #    return RedirectResponse(url="/auth", status_code=status.HTTP_302_FOUND)
+
+    todo_model = models.Todos()
+    todo_model.title = title
+    todo_model.description = description
+    todo_model.priority = priority
+    todo_model.complete = False
+    todo_model.owner_id = 1
+
+    db.add(todo_model)
+    db.commit()
+
+    return RedirectResponse(url="/todos", status_code=status.HTTP_302_FOUND)
+    
+
+
 @router.get("/edit-todo/{todo_id}", response_class=HTMLResponse)
 async def edit_todo(request: Request, todo_id: int, db: Session = Depends(get_db)):
-    return templates.TemplateResponse("edit-todo.html", {"request": request})
+
+    todo = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+
+    return templates.TemplateResponse("edit-todo.html", {"request": request, "todo": todo})
 
 
 """
